2539c3e58078c1282662cf74fee91cc666862add
Moticob <97236123+Moticob@users.noreply.github.com>
2024-08-18 10:53:39 -0700

    Create config.h

---
+++ config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID "Your_SSID"
+#define WIFI_PASSWORD "Your_PASSWORD"
+#define API_KEY "Your_API_KEY"
+#define SERVER_URL "http://api.thingspeak.com/update"
+
+#endif
